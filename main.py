c7cb79dcecd55de59af141e1db88011417f55348
Mikah Barnett <mikahb@meshtronics.com>
2019-11-01 11:01:53 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from listener import ReconnectingExampleConsumer
+
+async def Random():
+    while True:
+        await asyncio.sleep(2)
+        print('[*] Random!')
+
+async def Listener():
+    uri = 'amqp://guest:guest@localhost:5672/%2F'
+    listener = ReconnectingExampleConsumer(uri)
+    listener.run()
+
+async def main():
+    random_task = Random()
+    listen_task = Listener()
+    await asyncio.gather(random_task, listen_task)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
